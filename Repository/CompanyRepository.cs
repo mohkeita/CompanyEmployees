5e51972fe65d745f78c14b0e67a3d2c17497c50e
Mohamed Keita <mohkeita13@gmail.com>
2021-04-04 19:01:44 +0200

    Create repository class library and implement

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,14 @@
+using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext) 
+            : base(repositoryContext)
+        {
+        }
+    }
+}
