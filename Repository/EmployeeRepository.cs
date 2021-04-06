0ea74658937e8b00aa3a6eb35539eb0522e5e5ac
Mohamed Keita <mohkeita13@gmail.com>
2021-04-06 20:43:00 +0200

    Implement Searching

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -7,6 +7,7 @@ using Entities;
 using Entities.Models;
 using Entities.RequestFeatures;
 using Microsoft.EntityFrameworkCore;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -22,6 +23,8 @@ namespace Repository
             bool trackChanges)
         {
             var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
+                .Search(employeeParameters.SearchTerm)
                 .OrderBy(e => e.Name)
                 .ToListAsync();
 
